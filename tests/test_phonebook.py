a018b6d87ef29bd0d8a5d424caea22443ad3f203
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-08-15 23:35:36 -0700

    Added is_consistent method and added additional unit tests -- incomplete -- 2 failing tests

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -4,17 +4,63 @@ from phonebook import Phonebook
 
 
 class PhonebookTest(unittest.TestCase):
-    def test_create_phonebook(self):
-        phonebook = Phonebook()
+    def setUp(self):
+        # arrange
+        self.phonebook = Phonebook()
 
     def test_lookup_entry_by_name(self):
         # arrange
-        phonebook = Phonebook()
-        expected = "12345"
-        phonebook.add("Bob", expected)
+        expected_number = "12345"
+        name = "Bob"
+        self.phonebook.add(name, expected_number)
 
         # act
-        actual = phonebook.lookup("Bob")
+        actual_number = self.phonebook.lookup(name)
 
         # assert
-        self.assertEqual(expected, actual)
+        self.assertEqual(expected_number, actual_number)
+
+    def test_missing_entry_raises_KeyError(self):
+        # act and assert
+        with self.assertRaises(KeyError):
+            self.phonebook.lookup("missing")
+
+    def test_empty_phonebook_is_consistent(self):
+        # act
+        phonebook_is_consistent = self.phonebook.is_consistent()
+
+        # assert
+        self.assertTrue(phonebook_is_consistent)
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
+        # arrange
+        self.phonebook.add("Bob", "123435")
+        self.phonebook.add("Mary", "012345")
+
+        # act
+        phonebook_is_consistent = self.phonebook.is_consistent()
+
+        # assert
+        self.assertTrue(phonebook_is_consistent)
+
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        # arrange
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "12345")
+
+        # act
+        phonebook_is_consistent = self.phonebook.is_consistent()
+
+        # assert
+        self.assertFalse(phonebook_is_consistent, "Should be False because phonebook has duplicate phone number")
+
+    def test_phonebook_with_numbers_that_prefix_one_another_is_inconsistent(self):
+        # arrange
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "123")
+
+        # act
+        phonebook_is_consistent = self.phonebook.is_consistent()
+
+        # assert
+        self.assertFalse(phonebook_is_consistent, "Should be False because phonebook contains prefixed numbers")
